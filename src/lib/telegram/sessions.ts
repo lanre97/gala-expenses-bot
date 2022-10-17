8663de1db5e051e89ec8405da27449350ee0438c
Flavio <development@multilingualint.com>
2022-10-17 00:45:47 -0500

    hotfix/build-local-session

--- src/lib/telegram/sessions.ts
+++ src/lib/telegram/sessions.ts
@@ -1,12 +1,14 @@
-import LocalSession from "telegraf-session-local";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const LocalSession = require("telegraf-session-local");
 
 export default function getSession() {
   return new LocalSession({
     database: "sessions.json",
     storage: LocalSession.storageFileAsync,
     format: {
-      serialize: (obj) => JSON.stringify(obj, null, 2), // null & 2 for pretty-formatted JSON
-      deserialize: (str) => JSON.parse(str),
+      serialize: (obj: any) => JSON.stringify(obj, null, 2), // null & 2 for pretty-formatted JSON
+      deserialize: (str: string) => JSON.parse(str),
     },
     state: { action: null, data: {} },
   });
